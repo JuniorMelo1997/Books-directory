c06f14eafb81df5dca8abce191a295a4a9a06a6c
Genival Melo Junior <genivaldemelojr@gmail.com>
2022-07-23 20:14:16 -0300

    connection with database

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import { db } from "./database/dbConnections";
 
 const app = express();
 
-app.listen(3000, ()=>{
+app.listen(3000, async()=>{
+    await db.sync();
     console.log("App is running");
 })
